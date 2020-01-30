1feeafb45a1295f9d5ba6bc54c5719cb07927ba6
Guilherme Mertens <gvmertens@gmail.com>
2020-01-30 16:21:01 -0300

    Pqnas mudancas

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//Armazenamento de dados
+const projetos = [];
+
+//Midleware de interceptacao
+
+
+//Rotas 
+
+server.listen(3000);
